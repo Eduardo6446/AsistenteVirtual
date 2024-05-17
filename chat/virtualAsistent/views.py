121dd1ed35dbc1d1459a01945e2fed16390fb5ea
Eduardo6446 <eduorozco8@gmail.com>
2024-05-16 20:06:13 -0600

    feat: Add options to the chat form

--- chat/virtualAsistent/views.py
+++ chat/virtualAsistent/views.py
@@ -1,27 +1,30 @@
+# assistant/views.py
+
 from django.shortcuts import render
+from django.http import HttpResponse
 from .models import Message
 import spacy
 from datetime import datetime
-
 
 nlp = spacy.load("en_core_web_sm")
 
 def generate_response(user_input):
     doc = nlp(user_input)
-    # Logica basica para respuestas, esto puede ser tan complejo como necesites
-    if any(token.text.lower() in ["hello", "hi"] for token in doc):
-        return "Hello! How can I help you today?"
-    elif "time" in user_input.lower():
-        return f"The current time is {datetime.now().strftime('%H:%M:%S')}."
+    if "option1" in user_input.lower():
+        return f"que onda"
+    elif "option2" in user_input.lower():
+        return f"fecha: {datetime.now()}"
     else:
         return "I'm sorry, I don't understand that yet."
 
 def index(request):
     if request.method == "POST":
         user_input = request.POST.get("user_input")
-        bot_response = generate_response(user_input)
-        message = Message.objects.create(user_input=user_input, bot_response=bot_response)
+        # Procesar los datos específicos según el valor del botón
+
+        response = generate_response(user_input)
+        message = Message.objects.create(user_input=user_input, bot_response=response)
     else:
         message = None
-    
+
     return render(request, 'assistant/index.html', {'message': message})
